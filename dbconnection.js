66aed7563ec0ab87cb783aec42661f82f82b13f2
Praj13 <rajbansiprajwol@gmail.com>
2025-02-11 07:37:18 +0545

    data inserted into database

--- dbconnection.js
+++ dbconnection.js
@@ -3,10 +3,16 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: ""
+  password: "",
+  database:"training"
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+  var sql="Insert into User (name,email) values ('Prajwol','rajbansiprajwol@gmail.com')";
+  con.query(sql,function(err,result){
+    if(err) throw err;
+    console.log("data inserted");
+  })
 });
