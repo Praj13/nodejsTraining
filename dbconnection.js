93b97c62d38383dc058ea98de83e2b4c42379392
Praj13 <rajbansiprajwol@gmail.com>
2025-02-11 07:57:43 +0545

    selection of data

--- dbconnection.js
+++ dbconnection.js
@@ -10,9 +10,16 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql="Insert into User (name,email) values ('Prajwol','rajbansiprajwol@gmail.com')";
-  con.query(sql,function(err,result){
+//   var sql="Insert into User (name,email) values ('Prajwol','rajbansiprajwol@gmail.com')";
+//   var sql="Insert into User (name,email) values ('Yogesh','yogesh@gmail.com')";
+
+//   con.query(sql,function(err,result){
+//     if(err) throw err;
+//     console.log("data inserted");
+//   })
+  con.query("Select * from User",function(err,result,fields){
     if(err) throw err;
-    console.log("data inserted");
-  })
+    console.log(result);
+    })
 });
+
